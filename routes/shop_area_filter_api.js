4ef5071e94302d94c676781e5d4d70c74f4c9ab7
Najisu <s2322@e-mirim.hs.kr>
2025-02-18 00:48:46 +0900

    [fix] 주석 수정

--- routes/shop_area_filter_api.js
+++ routes/shop_area_filter_api.js
@@ -8,6 +8,7 @@ const key = {
     apiKey: process.env.API_KEY,
     uuid: '3ed8e58c-3256-464f-bec4-63925ebfae75'        
 }
+
 const router = express.Router();
 
 router.get('/filter/:apikey/:search_area', async function(req, res, next) {
@@ -19,16 +20,16 @@ router.get('/filter/:apikey/:search_area', async function(req, res, next) {
             return res.status(401).send('apikey is not valid.');
         }
     
-        // 지역 및 상점 데이터 조회
+        // 지역 및 식당 데이터 조회
         const [area] = await db.query('SELECT id, area_id, shop_name FROM shop');
         console.log("DB 데이터:", area); // 데이터 구조 확인
         console.log("요청한 지역:", search_area); // 찾는 데이터 확인
 
         if (!area || area.length === 0) {
             return res.status(404).json({ message: "No data found in database." });
-        }
+        }   
 
-        // 검색한 지역과 일치하는 가게만 필터링
+        // 선택한 지역과 일치하는 식당만 필터링
         const filteredShops = area
             .filter(item => item.area_id === Number(search_area));
 
