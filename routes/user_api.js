c99692d1a4be2cc3ce7597ed60984cf295a17e7b
Najisu <s2322@e-mirim.hs.kr>
2025-05-25 04:46:07 +0900

    [feat] 회원정보 조회 및 수정 기능 구현 #12

--- routes/user_api.js
+++ routes/user_api.js
@@ -177,4 +177,70 @@ router.post('/logout/:apikey', (req, res) => {
     });
 });
 
+// 회원 정보 조회
+router.get('/info/:apikey', async (req, res) => {
+
+    const user_id = req.session.user?.id;
+    console.log(user_id);
+
+    try {
+        const [user] = await db.query('SELECT user_name, user_mail, user_allergy FROM user WHERE user.id = ?', [user_id]);
+
+        if(!user){
+            return res.status(404).send({ error: "사용자 정보를 찾을 수 없습니다." });
+        }
+
+        res.status(200).send(user[0]);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({err: 'Database error'});
+    }
+});
+
+// 회원 정보 수정
+router.patch('/update/:apikey', async (req, res) => {
+    try{
+        const { apikey } = req.params;
+        const user_id = req.session.user?.id;
+        console.log(user_id);
+        const { name, mail, allergy } = req.body;
+
+        // API 키 검증
+        if (!uuidAPIKey.isAPIKey(apikey) || !uuidAPIKey.check(apikey, key.uuid)) {
+            return res.status(401).send('apikey is not valid.');
+        }
+
+        // 수정할 필드만 동적으로 구성
+        const fields = [];
+        const values = [];
+        
+        if(name) {
+            fields.push('user_name = ?');
+            values.push(name);
+        }
+        if(mail) {
+            fields.push('user_mail = ?');
+            values.push(mail);
+        }
+        if(allergy) {
+            fields.push('user_allergy = ?');
+            values.push(allergy);
+        }
+
+        if (fields.length === 0) {
+            return res.status(400).send({ err: '수정할 정보가 없습니다.' });
+        }
+
+        values.push(user_id);
+
+        const query = `UPDATE user SET ${fields.join(', ')} WHERE id = ?`;
+        await db.query(query, values);
+        return res.status(200).send({ message: '회원정보가 수정되었습니다!' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({err: 'Database error'});
+    }
+});
+
 module.exports = router;
