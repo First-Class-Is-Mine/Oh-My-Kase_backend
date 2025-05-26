9b4c64d652b35cb28e346fdf9df6f18bc8bb6ebe
Najisu <s2322@e-mirim.hs.kr>
2025-05-26 23:25:12 +0900

    [feat] 북마크 조회 기능 #15

--- routes/boomark_api.js
+++ routes/boomark_api.js
@@ -56,4 +56,51 @@ router.delete('/delete/:apikey/:bookmark_id', async (req, res) => {
     }
 });
 
+// user 북마크 목록 확인
+router.get('/user/:apikey', async (req, res) => {
+    try {
+        const { apikey } = req.params;
+        const user_id = req.session.user?.id;
+
+        //API 키 검증
+        if (!uuidAPIKey.isAPIKey(apikey) || !uuidAPIKey.check(apikey, key.uuid)) {
+            return res.status(401).send('apikey is not valid.');
+        }
+        if (!user_id) {
+            return res.status(400).send({ error: 'user_id가 존재하지 않습니다.' });
+        }
+
+        const [row] = await db.query('SELECT * FROM shop WHERE id IN (SELECT shop_id FROM bookmark WHERE user_id = ?)', [user_id]);
+        const bookmark_list = row[0];
+        return res.status(200).json({ bookmark_list });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Database error' });
+    }
+});
+
+// shop의 bookmark 갯수
+router.get('/shop/:apikey/:shop_id', async (req, res) => {
+    try {
+        const { apikey, shop_id } = req.params;
+
+        //API 키 검증
+        if (!uuidAPIKey.isAPIKey(apikey) || !uuidAPIKey.check(apikey, key.uuid)) {
+            return res.status(401).send('apikey is not valid.');
+        }
+        if (!shop_id) {
+            return res.status(400).send({ error: 'shop_id가 존재하지 않습니다.' });
+        }
+
+        const [row] = await db.query('SELECT COUNT(*) AS cnt FROM bookmark WHERE shop_id = ?', [shop_id]);
+        const bookmark_sum = row[0].cnt;
+        return res.status(200).json({ bookmark_sum });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Database error' });
+    }
+});
+
 module.exports = router;
